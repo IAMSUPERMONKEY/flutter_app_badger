448a21880967da453f787fd31c11315298a0db1e
liwenbo <liwenbo@supermonkey.com.cn>
2022-04-06 14:41:03 +0800

    fix: 增加 initConfig 方法(配合应用商店的隐私政策要求)

--- lib/flutter_app_badger.dart
+++ lib/flutter_app_badger.dart
@@ -1,10 +1,17 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/services.dart';
 
 class FlutterAppBadger {
-  static const MethodChannel _channel =
-      const MethodChannel('g123k/flutter_app_badger');
+  static const MethodChannel _channel = const MethodChannel('g123k/flutter_app_badger');
+
+  /// only Android
+  static void initConfig() {
+    if (Platform.isAndroid) {
+      _channel.invokeMethod('initConfig');
+    }
+  }
 
   static void updateBadgeCount(int count) {
     _channel.invokeMethod('updateBadgeCount', {"count": count});
